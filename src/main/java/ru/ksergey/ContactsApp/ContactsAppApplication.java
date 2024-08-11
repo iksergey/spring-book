47feada7a57f88242d5fa7b4d29ca3298c828453
iksergey <i@ksergey.ru>
2024-08-11 23:04:28 +0300

    Базовый проект

---
+++ src/main/java/ru/ksergey/ContactsApp/ContactsAppApplication.java
@@ -0,0 +1,13 @@
+package ru.ksergey.ContactsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ksergey/ContactsApp/ContactsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ksergey.ContactsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
