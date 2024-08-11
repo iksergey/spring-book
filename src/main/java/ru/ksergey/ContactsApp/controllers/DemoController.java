d8429071bb34c8a7a8934f211675a6054ed9f4d5
iksergey <i@ksergey.ru>
2024-08-12 00:37:39 +0300

    Демонстрация http post запросов

--- src/main/java/ru/ksergey/ContactsApp/controllers/DemoController.java
+++ src/main/java/ru/ksergey/ContactsApp/controllers/DemoController.java
@@ -3,6 +3,8 @@ package ru.ksergey.ContactsApp.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,18 @@ public class DemoController {
     @GetMapping("/{id}")
     public ResponseEntity<String> getItem(@PathVariable("id") String value) {
         String response = String.format("Передано значение: %s", value);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/postItem")
+    public ResponseEntity<String> postItemWithValue(@RequestBody String data) {
+        String response = String.format("postItemWithValue получил данные: %s", data);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postItem(@RequestBody String data) {
+        String response = String.format("postItem получил данные: %s", data);
         return ResponseEntity.ok(response);
     }
 
