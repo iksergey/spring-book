377aa9802a8a86cfc7603d2635724f98c61b83ae
iksergey <i@ksergey.ru>
2024-08-17 03:50:49 +0300

    Добавление Swagger-ui

--- src/main/java/ru/ksergey/ContactsApp/controllers/ContactController.java
+++ src/main/java/ru/ksergey/ContactsApp/controllers/ContactController.java
@@ -1,5 +1,9 @@
 package ru.ksergey.ContactsApp.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +14,21 @@ import ru.ksergey.ContactsApp.service.ContactService;
 
 @RestController
 @RequestMapping("/contact")
+@Tag(name = "Contact'ы", description = "Описание")
 public class ContactController {
 
     @Autowired
     private ContactService contactService;
 
+    @Operation(summary = "Получить случайный контакт", description = "Возвращает случайный контакт из базы данных.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Успешное получение контакта"),
+        @ApiResponse(responseCode = "404", description = "Контакт не найден"),
+        @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера")})
     @GetMapping(value = "/")
     public ResponseEntity<Contact> getRandomContact() {
         Contact contact = contactService.getContact();
         return ResponseEntity.ok(contact);
     }
 }
+
