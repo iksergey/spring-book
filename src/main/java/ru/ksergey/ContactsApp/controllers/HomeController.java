517ff2097abc91bebe9fa66328d8f937b7996be6
login-nigol <vadim.antipov@gmx.de>
2024-08-22 15:10:41 +0200

    index.html is renamed to start.html. HomeController class is created in the controllers package.

---
+++ src/main/java/ru/ksergey/ContactsApp/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package ru.ksergey.ContactsApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "start.html";
+    }
+}
