8798b2e44f6330530020cd8a723a11ba0df10148
login-nigol <vadim.antipov@gmx.de>
2024-08-24 11:20:40 +0200

    second step

--- src/main/java/ru/ksergey/ContactsApp/controllers/HomeController.java
+++ src/main/java/ru/ksergey/ContactsApp/controllers/HomeController.java
@@ -4,10 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class HomeController { // MVC (Model-View-Controller)
 
-    @GetMapping("/")
-    public String home() {
+    @GetMapping("/api/")
+    public String home() { // вызываемый метод
         return "start.html";
     }
 }
+
+//MVC (Model-View-Controller) играет роль посредника между моделью (данными) и представлением (интерфейсом пользователя).
+
+//Аннотация @Controller обрабатывают HTTP-запросы и возвращают представления
+// (например, HTML-страницы) или данные (например, JSON).
+
+//Аннотация @GetMapping("/") используется для маппинга HTTP GET-запросов на определенный метод контроллера. В данном
+// случае, она указывает, что метод будет обрабатывать GET-запросы, направленные на URL (/api/) вашего веб-приложения.
+// @GetMapping является специализированной формой аннотации @RequestMapping
