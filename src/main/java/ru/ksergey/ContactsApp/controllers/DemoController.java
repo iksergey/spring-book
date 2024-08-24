5cdb0cb7474e7cbbcb465512eefc1b890e23c1e8
login-nigol <vadim.antipov@gmx.de>
2024-08-24 12:54:01 +0200

    third step

---
+++ src/main/java/ru/ksergey/ContactsApp/controllers/DemoController.java
@@ -0,0 +1,37 @@
+package ru.ksergey.ContactsApp.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getItemById(@PathVariable("id") String value) {
+        String response = String.format("Передано значение: %s", value);
+        return ResponseEntity.ok(response);
+    }
+
+}
+
+
+//@RestController является сочетанием аннотаций @Controller и @ResponseBody, что означает, что все методы в
+// этом классе будут возвращать данные напрямую в HTTP-ответ, а не представление.
+
+//@RequestMapping(“/demo”) используется для задания базового URL для всех методов в контроллере, все методы в
+// DemoController будут доступны по URL, начинающемуся с /demo.
+
+//@GetMapping используется для обработки HTTP GET запросов. Путь /{id} указывает, что метод будет принимать
+// переменную часть URL, которая будет передана в метод в качестве параметра.
+
+//@PathVariable связывает переменную из URL с параметром метода.
+//В данном случае, значение из {id} в URL будет передано в метод как параметр value.
+
+//String.format("Передано значение: %s", value) формирует строку ответа с использованием значения переменной value.
+//Если значение value равно "привет!", то результатом этой строки будет "Передано значение: привет!".
+
+//ResponseEntity.ok() создает объект ResponseEntity с HTTP статусом 200 (OK), указывает на успешное выполнение запроса.
+
+// http://localhost:11000/demo/привет!
--- src/main/java/ru/ksergey/ContactsApp/controllers/HomeController.java
+++ src/main/java/ru/ksergey/ContactsApp/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController { // MVC (Model-View-Controller)
 
-    @GetMapping("/api/")
+    @GetMapping("/")
     public String home() { // вызываемый метод
         return "start.html";
     }
